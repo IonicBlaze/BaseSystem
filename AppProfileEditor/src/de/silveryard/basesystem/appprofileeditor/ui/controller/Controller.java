20be276ddfebbf84bec27d585166abcf3ae0a792
Christian Hofmann <c.hofmann@live.de>
2017-07-21 22:34:54 +0200

    Init App Profile Creator

---
+++ AppProfileEditor/src/de/silveryard/basesystem/appprofileeditor/ui/controller/Controller.java
@@ -0,0 +1,13 @@
+package de.silveryard.basesystem.appprofileeditor.ui.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable{
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("WOEHY INIT");
+    }
+}
